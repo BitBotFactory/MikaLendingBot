922b6116f280b8c821b658f47c078208da177344
Raanan <rnevet@gmail.com>
2017-02-09 22:21:03 +0100

    Data.truncate should always return a float

--- modules/Data.py
+++ modules/Data.py
@@ -136,6 +136,6 @@ def truncate(f, n):
     # From https://stackoverflow.com/questions/783897/truncating-floats-in-python
     s = '{}'.format(f)
     if 'e' in s or 'E' in s:
-        return '{0:.{1}f}'.format(f, n)
+        return float('{0:.{1}f}'.format(f, n))
     i, p, d = s.partition('.')
     return float('.'.join([i, (d + '0' * n)[:n]]))
