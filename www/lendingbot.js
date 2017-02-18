5ef0c33163f7beb7262d75a579a73ca934030aea
Raanan <rnevet@gmail.com>
2017-02-18 23:51:59 +0100

    fixed display of low values like 1e-7 (#238)

--- www/lendingbot.js
+++ www/lendingbot.js
@@ -53,11 +53,9 @@ function updateOutputCurrency(outputCurrency){
 // below zero accuracy will indicate precision after must significat digit
 // strips trailing zeros
 function prettyFloat(value, accuracy) {
-    var precision = Math.round(Math.log10(value));
-    precision = precision < 0 ? Math.abs(precision) + accuracy : accuracy;
-    var multiplier = Math.pow(10, precision);
-    var result = Math.round(value * multiplier) / multiplier;
-    return isNaN(result) ? "0" : parseFloat(result.toFixed(precision));
+  var precision = Math.round(Math.log10(value));
+  var result = precision < 0 ? value.toFixed(Math.min((accuracy - precision), 8)) : value.toFixed(accuracy);
+  return isNaN(result) ? "0" : result.replace(/(?:\.0+|(\.\d+?)0+)$/, "$1");
 }
 
 function printFloat(value, precision) {
