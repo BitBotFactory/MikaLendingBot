e357416f89dd2765c9756d2302931714832fd7d6
Michael Robinson <laxdog@gmail.com>
2017-12-02 17:16:17 +0000

    Add config end point

--- modules/FlaskServer.py
+++ modules/FlaskServer.py
@@ -1,17 +1,19 @@
 from flask import Flask, send_file
+import pandas as pd
 import threading
 
 app = Flask(__name__, static_url_path='', static_folder="../www")
 
 
 class FlaskServer(object):
-    def __init__(self, config):
+    def __init__(self, botConf):
         '''
         Setup the web server, retrieving the configuration parameters
         and starting the web server thread
         '''
+        self.botConf = botConf
         # Check for custom web server address
-        compositeWebServerAddress = config.get('BOT', 'customWebServerAddress', '0.0.0.0').split(":")
+        compositeWebServerAddress = botConf.get('BOT', 'customWebServerAddress', '0.0.0.0').split(":")
 
         # associate web server ip address
         self.web_server_ip = compositeWebServerAddress[0]
@@ -22,13 +24,15 @@ class FlaskServer(object):
             self.web_server_port = compositeWebServerAddress[1]
         else:
             # Check for custom web server port
-            self.web_server_port = config.get('BOT', 'customWebServerPort', '8000')
+            self.web_server_port = botConf.get('BOT', 'customWebServerPort', '8000')
 
         # Check for custom web server template
-        self.web_server_template = config.get('BOT', 'customWebServerTemplate', 'www')
+        self.web_server_template = botConf.get('BOT', 'customWebServerTemplate', 'www')
 
         print('Starting WebServer at {0} on port {1} with template {2}'
               .format(self.web_server_ip, self.web_server_port, self.web_server_template))
+
+        app.add_url_rule('/config', 'config', self.config)
 
     def run_web_server(self):
         self.thread_stop = threading.Event()
@@ -42,6 +46,13 @@ class FlaskServer(object):
     @app.route('/')
     def root():
         return send_file('../www/lendingbot.html')
+
+    def config(self):
+        df = pd.DataFrame(data=self.botConf.config.items('BOT'))
+        df = df.fillna(' ').T
+        df = df.transpose()
+        df.columns = ['Option', 'Value']
+        return df.to_html()
 
     def main(self, stop_event):
         while not stop_event.is_set():
